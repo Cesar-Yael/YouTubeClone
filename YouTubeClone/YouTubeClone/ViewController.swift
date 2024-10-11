a506ff2cb308d282a8cd5dccac8cf66d36aba1e2
cesarsoriano <CYSORIANO@sky.com.mx>
2024-10-11 10:57:52 -0600

    > Create the project in XCode

---
+++ YouTubeClone/YouTubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by Cesar Yael Soriano Ramirez on 11/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
